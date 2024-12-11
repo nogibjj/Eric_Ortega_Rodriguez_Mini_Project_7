c2aa23568493067be2305dd1682d86e47d164c01
Eric Ortega Rodriguez <ericiortega85@gmail.com>
2024-12-11 18:55:51 +0000

    new

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
-
-fn multiply(x: &i32, y: &i32) -> i32 {
-    x * y
+use clap::Parser;
+/// A basic example of using clap
+#[derive(Parser)]
+#[command(name = "basic_cli")]
+#[command(about = "A basic CLI example using clap", long_about = None)]
+struct Cli {
+    /// Name of the person to greet
+    name: String,
 }
-
 fn main() {
-    let a = 10;
-    let b = 20;
-    let result = multiply(&a, &b);
-    println!("The multiplication of {} and {} is {}", a, b, result);
+    let cli = Cli::parse();
+    println!("Hello, {}!", cli.name);
 }
